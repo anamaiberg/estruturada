efb701bcafc62e66e8c047dd7f02acbc1aac0b9c
Ana Clara Maiberg <108688584+anamaiberg@users.noreply.github.com>
2024-09-21 16:24:56 -0300

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float Maior(float num1, float num2){
+    if(num1>num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+
+int main(){
+
+    int i, n, maior, numeros[10];
+
+    do{
+        printf("Quantos numeros quieres informar? ");
+        scanf("%d", &n);
+    }while(n>10);
+
+    for(i=0; i<n; i++){
+        printf("Informe um numero: ");
+        scanf("%d", &numeros[i]);
+    }
+
+    maior = numeros[0];
+
+    for(i=1; i<n; i++){
+        maior = Maior(maior, numeros[i]);
+    }
+
+    printf("O maior numero e: %d", maior);
+
+    return 0;
+}
