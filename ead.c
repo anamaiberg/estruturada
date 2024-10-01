2704aa6eb0bd04233e500f72c38eb6026780f571
Ana Clara Maiberg <108688584+anamaiberg@users.noreply.github.com>
2024-10-01 13:10:19 -0300

    Update ead.c

--- ead.c
+++ ead.c
@@ -24,7 +24,7 @@ int verificar_numero(int numero){
 
 int main()
 {
-    int positivos[100], negativos[100], opcao, numero, qtd_positivos=0, qtd_negativos=0;
+    int i, positivos[100], negativos[100], opcao, opcao_vetor, numero, numero_antigo, numero_novo, qtd_positivos=0, qtd_negativos=0, corresponde=0;
     
     printf("Opcoes:\n1- Inserir um numero\n2- Imprimir um vetor\n3-Substituir um numero no vetor\n4- Sair\nEscolha uma opcao: ");
     scanf("%d", &opcao);
@@ -41,16 +41,51 @@ int main()
                 negativos[qtd_negativos] = numero;
                 qtd_negativos++;
             }
+            break;
         }
         
-        if(opcao==2){
+        else if(opcao==2){
             printf("Vetor de numeros positivos: ");
             imprimir_vetor(positivos, qtd_positivos);
             printf("Vetor de numeros negativos: ");
             imprimir_vetor(negativos, qtd_negativos);
         }
         
-        if(opcao==3){
+        else if(opcao==3){
+            printf("Voce deseja substituir um numero do vetor de positivos(1) ou do vetor de negativos(0)? ");
+            scanf("%d", &opcao_vetor);
+            printf("Qual numero desejas substituir? ");
+            scanf("%d", &numero_antigo);
+            
+            if(opcao_vetor==1){
+                for(i=0; i<qtd_positivos; i++){
+                    if(numero_antigo==positivos[i]){
+                        printf("Qual o novo numero? ");
+                        scanf("%d", &numero_novo);
+                        positivos[i] = numero_novo;
+                        corresponde=1;
+                        break;
+                    }
+                    if(corresponde==0){
+                        printf("Não há numero a ser substituido");
+                    }
+                }
+            }
+            else if(opcao_vetor==0){
+                for(i=0; i<qtd_negativos; i++){
+                    if(numero_antigo==negativos[i]){
+                        printf("Qual o novo numero? ");
+                        scanf("%d", &numero_novo);
+                        negativos[i] = numero_novo;
+                        corresponde=1;
+                        break;
+                    }
+                    if(corresponde==0){
+                        printf("Não há numero a ser substituido");
+                    }
+                }
+                
+            }
             
         }
     }
