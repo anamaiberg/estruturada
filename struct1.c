1354edacd6ab022f4202ebbaf58b7e65c2a3c3de
Ana Clara Maiberg <108688584+anamaiberg@users.noreply.github.com>
2024-10-17 19:29:38 -0300

    Update struct1.c

--- struct1.c
+++ struct1.c
@@ -15,28 +15,54 @@ struct Compromisso{
     char texto[100];
 };
 
-void imprimir(struct compromisso1){
-    printf("Compromisso:\nData: %d\nHorario: %d\nDescricao: %s", Compromisso.data, Compromisso.horario, Compromisso.texto);
+void imprimir(struct Compromisso UmCompromisso){
+    printf("Compromisso:\nData: %d/%d/%d\nHorario: %d:%d:%d\nDescricao: %s\n",
+           UmCompromisso.data.dia, UmCompromisso.data.mes, UmCompromisso.data.ano,
+           UmCompromisso.horario.hora, UmCompromisso.horario.minutos, UmCompromisso.horario.segundos, UmCompromisso.texto);
 }
 
+struct Compromisso Informar_compromisso(){
+    struct Compromisso local;
+
+    printf("Informe o dia: ");
+    scanf("%d", &local.data.dia);
+    printf("\nInforme o mes: ");
+    scanf("%d", &local.data.mes);
+    printf("\nInforme o ano: ");
+    scanf("%d", &local.data.ano);
+
+    printf("\nInforme a hora: ");
+    scanf("%d", &local.horario.hora);
+    printf("\nInforme os minutos: ");
+    scanf("%d", &local.horario.minutos);
+    printf("\nInforme os segundos: ");
+    scanf("%d", &local.horario.segundos);
+
+    printf("\nInforme a descricao: ");
+    scanf("%s", local.texto);
+
+    return local;
+};
 
 int main(){
-    struct Horario horario1;
-    horario1.hora = 12;
-    horario1.minutos = 50;
-    horario1.segundos = 33;
-
-    struct Data data1;
-    data1.ano = 2006;
-    data1.mes = 3;
-    data1.dia = 29;
-
     struct Compromisso compromisso1;
-    compromisso1.data = data1;
-    compromisso1.horario = horario1;
+    compromisso1.horario.hora = 12;
+    compromisso1.horario.minutos = 50;
+    compromisso1.horario.segundos = 33;
+    compromisso1.data.ano = 2006;
+    compromisso1.data.mes = 3;
+    compromisso1.data.dia = 29;
     strcpy(compromisso1.texto, "muito importante");
 
-    imprimir(comprmisso1);
+    struct Compromisso lista[10];
+
+    lista[0] = compromisso1;
+    lista[1] = Informar_compromisso();
+
+    for (int i=0; i<2; i++)
+    {
+        imprimir(lista[i]);
+    }
+
 
 }
-
