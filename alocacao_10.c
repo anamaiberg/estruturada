74c7c02db47f916bdc280a94c659902c90c797e5
Ana Clara Maiberg <108688584+anamaiberg@users.noreply.github.com>
2024-12-04 13:31:25 -0300

    Create alocacao_10.c

---
+++ alocacao_10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Atribue(double *vetor, int tamanho){
+    srand(time(0));
+    for(int i=0; i<10; i++){
+        vetor[i] = (rand() %100 + 1); 
+    }
+}
+
+void Imprime(double *vetor){
+    for(int i=0; i<10; i++){
+        printf("%f ", vetor[i]);
+    }
+}
+
+int main(){
+    double *vetor;
+    int tamanho;
+    
+    do{
+        printf("Quantos valores desejas armazenar? (Pelo menos 10)  ");
+        scanf("%d", &tamanho);
+    }
+    while(tamanho<10);
+    
+    
+    vetor = (double *) malloc(sizeof(double) * tamanho);
+    
+    Atribue(vetor, tamanho);
+    
+    printf("10 primeiros numeros do vetor: ");
+    Imprime(vetor);
+    
+}
